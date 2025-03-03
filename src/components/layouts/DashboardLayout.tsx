86d852b21f57a0be5b1cce43d9a5b53c0d62ba80
Rayyan Khan <rk4004104@gmail.com>
2025-03-03 12:04:42 +0500

    Enhance FeeDetails Component and Sidebar Functionality
    - Implemented collapsible sidebar for improved user experience.
    - Adjusted width of StatsCard for better layout consistency.
    - Added missing dropdowns in FeeDetails component to enhance functionality.
    - Integrated filter popup in FeeDetails for advanced filtering options.

--- src/components/layouts/DashboardLayout.tsx
+++ src/components/layouts/DashboardLayout.tsx
@@ -7,14 +7,24 @@ interface DashboardLayoutProps {
 }
 
 const DashboardLayout: React.FC<DashboardLayoutProps> = ({ children }) => {
+      const [isSideBarExpanded, setIsSideBarExpanded] = useState(false);
   return (
     <div className="flex relative w-full h-auto min-h-screen flex-col">
       <Navbar />
       <div className="w-full flex ">
-        <div className="w-[20%]">
-          <Sidebar />
+        <div
+          className={` ${
+            isSideBarExpanded
+              ? "w-[96px] duration-300 ease-in-out"
+              : "w-[20%] duration-300 ease-in-out"
+          }`}
+        >
+          <Sidebar
+            isSideBarExpanded={isSideBarExpanded}
+            setIsSideBarExpanded={setIsSideBarExpanded}
+          />
         </div>
-        <span className="w-[80%]">{children}</span>
+        <span className="grow">{children}</span>
       </div>
     </div>
   );
