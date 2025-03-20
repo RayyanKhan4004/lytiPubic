eca717c4e5c8719ec33007e5c1014445046920f8
Umar ali <umarcodev@gmail.com>
2025-03-20 10:45:41 +0500

    Refactor ExpandedComponentFeeType and FeeDetail components: clean up DataTable usage and improve styling

--- src/components/orders/ExpandedComponentFeeType.tsx
+++ src/components/orders/ExpandedComponentFeeType.tsx
@@ -1,3 +1,42 @@
+// import DataTable from "react-data-table-component";
+// import { formatNumberWithoutDecimals } from "../../utils/functions";
+
+// const ExpandedComponentFeeType = ({ data }: { data: any }) => (
+//   <div>
+//     <DataTable
+//       columns={[
+//         {
+//           name: "",
+//           selector: (row: any) => "",
+//           sortable: false,
+//           maxWidth: "30px",
+//         },
+//         {
+//           name: "",
+//           selector: (row: any) => row.account,
+//           sortable: false,
+//           maxWidth: "140px",
+//         },
+//         {
+//           name: "",
+//           selector: (row: any) => row.totalAmount,
+//           cell: (row: any) => formatNumberWithoutDecimals(row.totalAmount),
+//           sortable: false,
+//         },
+//       ]}
+//       data={data.accountSums}
+//       noHeader
+//       noDataComponent={
+//         <div className="text-gray-500">No accounts available</div>
+//       }
+//       striped
+//       highlightOnHover
+//     />
+//   </div>
+// );
+
+// export default ExpandedComponentFeeType;
+
 import DataTable from "react-data-table-component";
 import { formatNumberWithoutDecimals } from "../../utils/functions";
 
@@ -6,23 +45,46 @@ const ExpandedComponentFeeType = ({ data }: { data: any }) => (
     <DataTable
       columns={[
         {
-          name: "Account",
-          selector: (row: any) => row.account,
+          name: "",
+          selector: (row: any) => "",
           sortable: false,
+          maxWidth: "30px",
         },
         {
-          name: "Total Amount",
+          selector: (row: any) => row.account,
+          sortable: false,
+          maxWidth: "150px",
+        },
+        {
           selector: (row: any) => row.totalAmount,
           cell: (row: any) => formatNumberWithoutDecimals(row.totalAmount),
           sortable: false,
         },
       ]}
       data={data.accountSums}
+      noHeader
+      dense
       noDataComponent={
         <div className="text-gray-500">No accounts available</div>
       }
-      striped
       highlightOnHover
+      customStyles={{
+        headRow: {
+          style: {
+            display: "none",
+          },
+        },
+        rows: {
+          style: {
+            backgroundColor: "#f5f5f5",
+          },
+        },
+        cells: {
+          style: {
+            padding: "8px",
+          },
+        },
+      }}
     />
   </div>
 );
