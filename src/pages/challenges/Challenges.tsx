dea50c33c4cbe5981b543b5494b05e780dc55027
Umar ali <umarcodev@gmail.com>
2025-03-28 17:23:03 +0500

    Add challenge fetching and deletion functionality; navigate to challenges on creation

--- src/pages/challenges/Challenges.tsx
+++ src/pages/challenges/Challenges.tsx
@@ -1,255 +1,212 @@
 import { useState } from "react";
-import upload from "../../assets/icons/UploadSimple.svg";
 import add from "../../assets/icons/Add.svg";
-import menu from "../../assets/icons/Menu.svg";
-import arrowUpDown from "../../assets/icons/ArrowsDownUp.svg";
-import dummy from "../../assets/images/Dummy.jpg";
-
 import { useNavigate } from "react-router-dom";
 import Breadcrumb from "../../components/common/BreadCrumb";
-import SearchInput from "../../components/common/SearchInput";
-import UserActionsPopup from "../../components/admin/users/UserActionsPopup";
-import CustomizableDropdown from "../../components/common/CustomizableDropdown";
 import Pagination from "../../components/common/Pagination";
+import {
+  useDeleteChallengeMutation,
+  useGetChallengesQuery,
+} from "../../lib/rtkQuery/challengeApi";
+import TablesSkeleton from "../../components/ui/skeleton/TablesSkeleton";
+import DataTable, { TableColumn } from "react-data-table-component";
+import MainTitle from "../../components/ui/typography/MainTitle";
+import CardLayout from "../../components/layouts/CardLayout";
+import SearchInput from "../../components/inputs/SearchInput";
+import Spinner from "../../components/ui/loader/Spinner";
+import PopoverMenu from "../../components/ui/popup/PopupMenu";
+import menu from "../../assets/icons/Menu.svg";
+import toast from "react-hot-toast";
 
 const Challenges = () => {
-  const [searchValue, setSearchValue] = useState("");
-  const [itemsPerPage, setItemsPerPage] = useState(10);
-  const [activeIndex, setActiveIndex] = useState<number | null>(null);
+  const [page, setPage] = useState(1);
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const [loading, setLoading] = useState("");
+
   const navigate = useNavigate();
 
-  const options: string[] = [
-    "10 Items Per Page",
-    "20 Items Per Page",
-    "30 Items Per Page",
-  ];
-  //   dummy data
-  const users = [
-    {
-      id: 6748,
-      name: "Ralph Edwards",
-      role: "Account Executive",
-      image: dummy,
-      phone: "(303) 555-0105",
-      added: "8/15/17",
-      lastAccess: "6/21/19",
-      status: "Inactive",
-      excluded: true,
-      downloadTrans: false,
-      activeTrans: 0,
-    },
-    {
-      id: 6749,
-      name: "John Doe",
-      image: dummy,
-      role: "Sales Manager",
-      phone: "(404) 555-0123",
-      added: "9/10/18",
-      lastAccess: "5/14/20",
-      status: "Active",
-      excluded: false,
-      downloadTrans: true,
-      activeTrans: 3,
-    },
-    {
-      id: 6748,
-      name: "Ralph Edwards",
-      role: "Account Executive",
-      image: dummy,
-      phone: "(303) 555-0105",
-      added: "8/15/17",
-      lastAccess: "6/21/19",
-      status: "Inactive",
-      excluded: true,
-      downloadTrans: false,
-      activeTrans: 0,
-    },
-    {
-      id: 6749,
-      name: "John Doe",
-      image: dummy,
-      role: "Sales Manager",
-      phone: "(404) 555-0123",
-      added: "9/10/18",
-      lastAccess: "5/14/20",
-      status: "Active",
-      excluded: false,
-      downloadTrans: true,
-      activeTrans: 3,
-    },
-    {
-      id: 6748,
-      name: "Ralph Edwards",
-      role: "Account Executive",
-      image: dummy,
-      phone: "(303) 555-0105",
-      added: "8/15/17",
-      lastAccess: "6/21/19",
-      status: "Inactive",
-      excluded: true,
-      downloadTrans: false,
-      activeTrans: 0,
-    },
-    {
-      id: 6749,
-      name: "John Doe",
-      image: dummy,
-      role: "Sales Manager",
-      phone: "(404) 555-0123",
-      added: "9/10/18",
-      lastAccess: "5/14/20",
-      status: "Active",
-      excluded: false,
-      downloadTrans: true,
-      activeTrans: 3,
-    },
-    {
-      id: 6748,
-      name: "Ralph Edwards",
-      role: "Account Executive",
-      image: dummy,
-      phone: "(303) 555-0105",
-      added: "8/15/17",
-      lastAccess: "6/21/19",
-      status: "Inactive",
-      excluded: true,
-      downloadTrans: false,
-      activeTrans: 0,
-    },
-    {
-      id: 6749,
-      name: "John Doe",
-      image: dummy,
-      role: "Sales Manager",
-      phone: "(404) 555-0123",
-      added: "9/10/18",
-      lastAccess: "5/14/20",
-      status: "Active",
-      excluded: false,
-      downloadTrans: true,
-      activeTrans: 3,
-    },
-    {
-      id: 6748,
-      name: "Ralph Edwards",
-      role: "Account Executive",
-      image: dummy,
-      phone: "(303) 555-0105",
-      added: "8/15/17",
-      lastAccess: "6/21/19",
-      status: "Inactive",
-      excluded: true,
-      downloadTrans: false,
-      activeTrans: 0,
-    },
-    {
-      id: 6749,
-      name: "John Doe",
-      image: dummy,
-      role: "Sales Manager",
-      phone: "(404) 555-0123",
-      added: "9/10/18",
-      lastAccess: "5/14/20",
-      status: "Active",
-      excluded: false,
-      downloadTrans: true,
-      activeTrans: 3,
-    },
-    {
-      id: 6748,
-      name: "Ralph Edwards",
-      role: "Account Executive",
-      image: dummy,
-      phone: "(303) 555-0105",
-      added: "8/15/17",
-      lastAccess: "6/21/19",
-      status: "Inactive",
-      excluded: true,
-      downloadTrans: false,
-      activeTrans: 0,
-    },
-    {
-      id: 6749,
-      name: "John Doe",
-      image: dummy,
-      role: "Sales Manager",
-      phone: "(404) 555-0123",
-      added: "9/10/18",
-      lastAccess: "5/14/20",
-      status: "Active",
-      excluded: false,
-      downloadTrans: true,
-      activeTrans: 3,
-    },
-    {
-      id: 6748,
-      name: "Ralph Edwards",
-      role: "Account Executive",
-      image: dummy,
-      phone: "(303) 555-0105",
-      added: "8/15/17",
-      lastAccess: "6/21/19",
-      status: "Inactive",
-      excluded: true,
-      downloadTrans: false,
-      activeTrans: 0,
-    },
-    {
-      id: 6749,
-      name: "John Doe",
-      image: dummy,
-      role: "Sales Manager",
-      phone: "(404) 555-0123",
-      added: "9/10/18",
-      lastAccess: "5/14/20",
-      status: "Active",
-      excluded: false,
-      downloadTrans: true,
-      activeTrans: 3,
-    },
-    {
-      id: 6748,
-      name: "Ralph Edwards",
-      role: "Account Executive",
-      image: dummy,
-      phone: "(303) 555-0105",
-      added: "8/15/17",
-      lastAccess: "6/21/19",
-      status: "Inactive",
-      excluded: true,
-      downloadTrans: false,
-      activeTrans: 0,
-    },
-    {
-      id: 6749,
-      name: "John Doe",
-      image: dummy,
-      role: "Sales Manager",
-      phone: "(404) 555-0123",
-      added: "9/10/18",
-      lastAccess: "5/14/20",
-      status: "Active",
-      excluded: false,
-      downloadTrans: true,
-      activeTrans: 3,
-    },
-  ];
-
-  const toggleDropdown = (index: number) => {
-    setActiveIndex(activeIndex === index ? null : index);
+  const { data, isLoading, refetch } = useGetChallengesQuery({
+    keyword: searchTerm,
+    page,
+    limit: 10,
+  });
+  const handlePageChange = ({ selected }: { selected: number }) => {
+    const newPage = selected + 1;
+    if (newPage >= 1 && newPage <= data?.totalPages) {
+      setPage(newPage);
+    }
   };
+  const handleSearch = (searchTerm: string) => {
+    setSearchTerm(searchTerm.toLowerCase());
+  };
+  const [deleteChallenge] = useDeleteChallengeMutation();
+  const handleAction = async (action: string, rowData: any) => {
+    if (action === "delete" && rowData) {
+      setLoading(rowData?.id);
+      try {
+        await deleteChallenge(rowData?.id).unwrap();
+
+        toast.success("Order deleted successfully");
+        refetch();
+      } catch (err: any) {
+        toast.error(err?.data?.message || "Cannot delete user");
+      } finally {
+        setLoading("");
+      }
+    }
+  };
+
+  const challengeColumns: TableColumn<any>[] = [
+    {
+      name: "",
+      cell: (row: any) =>
+        loading === row?.id ? (
+          <Spinner />
+        ) : (
+          <PopoverMenu
+            triggerImage={menu}
+            options={[
+              { label: "Edit", onClick: () => handleAction("edit", row) },
+              { label: "Detail", onClick: () => handleAction("detail", row) },
+              { label: "Delete", onClick: () => handleAction("delete", row) },
+            ]}
+          />
+        ),
+      ignoreRowClick: true,
+      allowOverflow: true,
+      button: true,
+    },
+    {
+      name: "Challenge Name",
+      selector: (row: any) => row.challengeName,
+      cell: (row: any) => (
+        <div className="rowStyle w-[200px]" title={row.challengeName}>
+          {row.challengeName}
+        </div>
+      ),
+      maxWidth: "200px",
+    },
+    {
+      name: "Category",
+      selector: (row: any) => row.category.categoryName,
+      cell: (row: any) => (
+        <div className="rowStyle w-[300px]" title={row.category.categoryName}>
+          {row.category.categoryName}
+        </div>
+      ),
+      maxWidth: "300px",
+    },
+    {
+      name: "Challenge Type",
+      selector: (row: any) => row.challengeType,
+      cell: (row: any) => (
+        <div className="rowStyle w-[200px]" title={row.challengeType}>
+          {row.challengeType}
+        </div>
+      ),
+      maxWidth: "200px",
+    },
+    {
+      name: "Start Date",
+      selector: (row: any) => row.startDate,
+      cell: (row: any) => (
+        <div className="rowStyle" title={row.startDate}>
+          {row.startDate}
+        </div>
+      ),
+    },
+    {
+      name: "End Date",
+      selector: (row: any) => row.endDate,
+      cell: (row: any) => (
+        <div className="rowStyle" title={row.endDate}>
+          {row.endDate}
+        </div>
+      ),
+    },
+    {
+      name: "Points",
+      selector: (row: any) => row.points,
+      cell: (row: any) => (
+        <div className="rowStyle" title={String(row.points)}>
+          {row.points}
+        </div>
+      ),
+    },
+  ];
+  // const challengeColumns: TableColumn<any>[] = [
+  //   {
+  //     name: "Challenge Name",
+  //     selector: (row: any) => row.challengeName,
+  //     cell: (row: any) => (
+  //       <div className="rowStyle w-[200px]" title={row.challengeName}>
+  //         {row.challengeName}
+  //       </div>
+  //     ),
+  //     maxWidth: "200px",
+  //   },
+  //   {
+  //     name: "Category",
+  //     selector: (row: any) => row.category.categoryName,
+  //     cell: (row: any) => (
+  //       <div className="rowStyle w-[200px]" title={row.category.categoryName}>
+  //         {row.category.categoryName}
+  //       </div>
+  //     ),
+  //     maxWidth: "200px",
+  //   },
+  //   {
+  //     name: "Challenge Type",
+  //     selector: (row: any) => row.challengeType,
+  //     cell: (row: any) => (
+  //       <div className="rowStyle w-[150px]" title={row.challengeType}>
+  //         {row.challengeType}
+  //       </div>
+  //     ),
+  //     maxWidth: "150px",
+  //   },
+  //   {
+  //     name: "Start Date",
+  //     selector: (row: any) => row.startDate,
+  //     cell: (row: any) => (
+  //       <div className="rowStyle w-[150px]" title={row.startDate}>
+  //         {row.startDate}
+  //       </div>
+  //     ),
+  //     maxWidth: "150px",
+  //   },
+  //   {
+  //     name: "End Date",
+  //     selector: (row: any) => row.endDate,
+  //     cell: (row: any) => (
+  //       <div className="rowStyle w-[150px]" title={row.endDate}>
+  //         {row.endDate}
+  //       </div>
+  //     ),
+  //     maxWidth: "150px",
+  //   },
+  //   {
+  //     name: "Points",
+  //     selector: (row: any) => row.points,
+  //     cell: (row: any) => (
+  //       <div className="rowStyle w-[100px]" title={String(row.points)}>
+  //         {row.points}
+  //       </div>
+  //     ),
+  //     maxWidth: "100px",
+  //   },
+  // ];
   return (
     <div className="w-full px-4 my-8">
       <Breadcrumb items={["Challenges"]} />
-      <div className="shadow-(--cardShadow) rounded-2xl bg-white w-full px-4 min-h-screen my-6">
+      <CardLayout>
         <div className="font-Poppins flex justify-between items-center w-full pt-3">
-          <h2 className="text-lg text-(--primary) font-semibold">Challenges</h2>
+          <MainTitle title="Challenges" />
           <div className="flex items-center gap-3">
             <SearchInput
-              value={searchValue}
-              onChange={(e: any) => setSearchValue(e.target.value)}
+              debounceTimeout={500}
+              placeholder="Search Keyword"
+              onChange={handleSearch}
             />
-
             <div
               className="bg-(--primary) flex items-center cursor-pointer gap-1.5 text-sm h-[44px] px-3 rounded-xl text-white"
               onClick={() => navigate("/create-challenge")}
@@ -260,112 +217,38 @@ const Challenges = () => {
           </div>
         </div>
 
-        <table className="w-full text-start font-Poppins text-sm font-normal text-[#15120F] mt-5">
-          <thead className="text-sm font-normal text-start ">
-            <tr className="border-b-[1px] border-[#F4EFE9] ">
-              <th className="text-start font-medium ">
-                <div className="flex  gap-2 items-center">
-                  Created Date <img src={arrowUpDown} alt="" />
-                </div>
-              </th>
-              <th className="text-start font-medium ">
-                <div className="flex  gap-2 items-center">
-                  Name <img src={arrowUpDown} alt="" />
-                </div>
-              </th>
-              <th className="text-start font-medium ">
-                <div className="flex  gap-2 items-center">
-                  Type <img src={arrowUpDown} alt="" />
-                </div>
-              </th>
-              <th className="text-start font-medium ">
-                <div className="flex  gap-2 items-center">
-                  Scope <img src={arrowUpDown} alt="" />
-                </div>
-              </th>
-              <th className="text-start font-medium ">
-                <div className="flex  gap-2 items-center">
-                  Start Date <img src={arrowUpDown} alt="" />
-                </div>
-              </th>
-              <th className="text-start font-medium ">
-                <div className="flex  gap-2 items-center">
-                  End Date <img src={arrowUpDown} alt="" />
-                </div>
-              </th>
-              <th className="text-start font-medium ">
-                <div className="flex  gap-2 items-center">
-                  Category <img src={arrowUpDown} alt="" />
-                </div>
-              </th>
-              <th className="text-start font-medium ">
-                <div className="flex  gap-2 items-center">
-                  Time period <img src={arrowUpDown} alt="" />
-                </div>
-              </th>
-              <th className="text-start font-medium ">
-                <div className="flex  gap-2 items-center">
-                  Points <img src={arrowUpDown} alt="" />
-                </div>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map(
-              (e: any, i: number) => (
-                <tr
-                  key={i}
-                  className="font-Jakarta text-sm font-normal text-[#15120F] h-[80px] border-b-[1px] border-[#F4EFE9]"
-                >
-                  <td
-                    className="cursor-pointer px-3 "
-                    style={{
-                      maxWidth: "50px",
-                      overflow: "hidden",
-                      whiteSpace: "nowrap",
-                      textOverflow: "ellipsis",
-                    }}
-                    title={e.id}
-                  >
-                    {e.id}
-                  </td>
-                  <td>
-                    <h3 className="font-medium ">{e.name}</h3>
-                  </td>
-                  <td>{e.phone}</td>
-                  <td>{e.added}</td>
-                  <td>{e.lastAccess}</td>
-                  <td>{e.status}</td>
-                  <td>{e.excluded ? "Yes" : "No"}</td>
-                  <td>{e.downloadTrans ? "Yes" : "No"}</td>
-                  <td>{e.activeTrans}</td>
-                  <td>
-                    <img
-                      src={menu}
-                      alt=""
-                      onClick={() => {
-                        toggleDropdown(i);
-                      }}
-                    />
-                    {activeIndex === i && <UserActionsPopup />}
-                  </td>
-                </tr>
-              ),
-              []
-            )}
-          </tbody>
-        </table>
-      </div>
+        <div className="w-full overflow-x-auto">
+          {isLoading ? (
+            <TablesSkeleton
+              columnCount={challengeColumns.length}
+              rowCount={10}
+            />
+          ) : (
+            <div className="w-full max-w-full">
+              <DataTable
+                columns={challengeColumns}
+                data={data?.data || []}
+                highlightOnHover
+                striped
+                className="head-row table-row"
+                noDataComponent={
+                  <div className="w-full text-center py-6 px-6 text-gray-500 bg-gray-100 rounded">
+                    No data found
+                  </div>
+                }
+                fixedHeader
+                fixedHeaderScrollHeight="700px"
+              />
+            </div>
+          )}
+        </div>
+      </CardLayout>
 
       <div className="w-full flex justify-end gap-5 items-center">
-        <CustomizableDropdown
-          options={options}
-          selected={`${itemsPerPage} Items Per Page`}
-          setSelected={() => ""}
-          width="w-60"
+        <Pagination
+          onPageChange={handlePageChange}
+          pageCount={data?.totalPages}
         />
-
-        <Pagination onPageChange={() => ""} pageCount={4} />
       </div>
     </div>
   );
