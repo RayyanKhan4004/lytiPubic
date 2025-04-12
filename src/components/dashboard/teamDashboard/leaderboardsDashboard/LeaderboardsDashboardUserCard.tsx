8be48c1915d8a5101774e1cf5ea9532f88b21d20
Umar ali <umarcodev@gmail.com>
2025-04-12 14:22:54 +0500

    Enhance Leaderboards functionality: update UserCard to display percentage and refactor LeaderBoards to integrate new leaderboard options and data mapping.

--- src/components/dashboard/teamDashboard/leaderboardsDashboard/LeaderboardsDashboardUserCard.tsx
+++ src/components/dashboard/teamDashboard/leaderboardsDashboard/LeaderboardsDashboardUserCard.tsx
@@ -5,6 +5,7 @@ type UserCardProps = {
   rank: number;
   name: string;
   count: number;
+  percentage?: number;
   width?: string;
   border?: string;
 };
@@ -14,9 +15,20 @@ const LeaderboardsDashboardUserCard: React.FC<UserCardProps> = ({
   rank,
   name,
   count,
+  percentage,
   width = "w-[48%]",
   border = "border border-white",
 }) => {
+  const rankColors: { [key: number]: string } = {
+    1: "#FE5100",
+    2: "#0098CE",
+    3: "#F8A219",
+    4: "#404041",
+    5: "#8775FF",
+  };
+
+  const badgeColor = rankColors[rank] || "#E0E0E0";
+
   return (
     <div
       className={`bg-white flex flex-col gap-1 rounded-xl p-3 ${border} ${width} items-center mt-4`}
@@ -28,13 +40,15 @@ const LeaderboardsDashboardUserCard: React.FC<UserCardProps> = ({
           className="w-[60px] h-[60px] rounded-full"
         />
         <div
-          className={`w-[24px] h-[24px] rounded-full flex justify-center items-center bg-(--secondary) text-white absolute left-0 mx-auto right-0 bottom-[-11px] text-base font-semibold `}
+          className="w-[24px] h-[24px] rounded-full flex justify-center items-center text-white absolute left-0 mx-auto right-0 bottom-[-11px] text-base font-semibold"
+          style={{ backgroundColor: badgeColor }}
         >
           {rank}
         </div>
       </div>
       <h2 className="font-bold text-sm">{name}</h2>
-      <h3 className="text-xs gap-2">Count: {count}</h3>
+      <h3 className="text-xs">Count: {count}</h3>
+      <h3 className="text-xs">{percentage} of total</h3>
     </div>
   );
 };
